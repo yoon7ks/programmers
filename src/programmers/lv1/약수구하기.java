0f0ba0adcad5da33998f2a60497a5fda94a74123
Yoonjin <yoon7ks@gmail.com>
2019-10-22 09:39:49 +0900

    레벨2 추가

--- src/programmers/lv1/약수구하기.java
+++ src/programmers/lv1/약수구하기.java
@@ -7,7 +7,7 @@ public class 약수구하기 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		solution(2999);
+		solution(5);
 	}
 // 자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
 /*
